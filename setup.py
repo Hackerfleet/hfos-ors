9727e5149f84871074a2f334d881bee3f5c25f83
ri0t <riot@c-base.org>
2021-02-22 19:13:32 +0100

    bumps version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hfos-ors",
-    version="0.0.1",
+    version="0.0.2",
     description="hfos-ors",
 
     author="Hackerfleet Community",
